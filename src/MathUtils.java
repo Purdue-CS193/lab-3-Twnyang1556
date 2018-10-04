cc44a62546410cfdda84a4abe3fba33769432f57
yang1556 <yang1556@purdue.edu>
2018-10-04 04:22:23 -0400

    made changes to codes

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -28,14 +28,13 @@ public class MathUtils {
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
-        for (int i = 0; i < exp; i++) {
-           res *= base; 
+        for (int i = 1; i < exp; i++) {
+           res *= base;
         }
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
-            return dividedRes;
+            return MathUtils.divideNumbers(1, res);
         }
         /* Otherwise, we are safe to just return the result */
         else {
